9e2beb3b0183719b219c1608ac95edbad9df15b7
wellsgz <wellsgz@hotmail.com>
2025-07-13 12:24:49 +0800

    Bump version to 0.1.1 to trigger release
    - Increment version from 0.1.0 to 0.1.1
    - This should trigger auto-tagging workflow
    - Which will create v0.1.1 tag and trigger release workflow
    - Release workflow will build and publish cross-platform binaries

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 // Version information
 const (
 	AppName    = "syslog-sender"
-	AppVersion = "0.1.0"
+	AppVersion = "0.1.1"
 	AppAuthor  = "https://github.com/wellsgz/syslog-sender"
 )
 
